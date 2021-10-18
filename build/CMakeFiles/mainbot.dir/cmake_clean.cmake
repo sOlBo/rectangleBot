7f2ed56296b10c60427d8173463cbb70bb0efd34
Eric Gediman <ericgediman@gmail.com>
2021-10-17 18:33:37 -0700

    added starting build system + code

---
+++ build/CMakeFiles/mainbot.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mainbot.dir/mainbot.cc.o"
+  "libmainbot.pdb"
+  "libmainbot.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mainbot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
